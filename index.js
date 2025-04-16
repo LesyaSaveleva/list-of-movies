daf4eb5a69dfa3a5abca8c9a12ba5b37fc394a54
Olesya Saveleva <sav9293563768@gmail.com>
2025-04-16 18:10:00 +0700

    Update index.js
    Сохранение списка фильмов в Local Storage. Проверка на ввод пользователем фильма

--- index.js
+++ index.js
@@ -6,17 +6,45 @@ const movieDeleteBtnNode = document.querySelectorAll('.delete_movie-btn-js');
 const movieTitle = document.querySelector('.movie_title');
 const movieTitleList = document.querySelector('.movie_title_list');
 
-const movies = [];
+let movies = [];
+const MOVIE_STORAGE = 'movie'
+
+// Загружаем список фильмов из Local Storage при открытии страницы
+document.addEventListener('DOMContentLoaded', ()=> {
+    loadMovies(); // Сразу загружаем фильмы из storage при старте страницы
+    renderMovie(movies); // Рендерим страницу с теми фильмами, которые были найдены
+    loadStyle();
+})
+
+// Функция загрузки фильмов из Local Storage
+function loadMovies() {
+const movieListFromStorageString = localStorage.getItem(MOVIE_STORAGE); // Получаем строку с фильмами
+if (movieListFromStorageString !== null && movieListFromStorageString.trim().length > 0) {
+    movies = JSON.parse(movieListFromStorageString); // Преобразовываем строковое представление обратно в массив объектов
+    renderMovie();
+}
+}
 
 // Добавление фильма в список принажатии Enter
 moviesInputNode.addEventListener('keyup', function(event){
     if (event.key === 'Enter') {
-        submitText();
+        if (!moviesInputNode.value) {
+            confirm('Введите название фильма')
+        } else{
+            submitText();
+            saveMovieList();
+        }
+        
     }
 })
 // Добавление фильма в список по кнопке
 addMoviesBtnNode.addEventListener('click', function(){
-    submitText();
+    if (!moviesInputNode.value) {
+        confirm('Введите название фильма')
+    } else{
+        submitText();
+        saveMovieList();
+    }
 })
 
 // Удаление фильма из списка с помощью делегирования событий на родительский элемент
@@ -25,6 +53,7 @@ listofmoviesNode.addEventListener('click', function(event) {
         const index = event.target.dataset.index;
         if (confirm(`Вы действительно хотите удалить фильм "${movies[index]}?"`)) {
             removeMovie(index);
+            saveMovieList();
         }
     }
 });
@@ -39,14 +68,39 @@ listofmoviesNode.addEventListener('click', function(event) {
             title.classList.remove('changeOfStyle-title');
             const parentDiv = btnClicked.parentElement; // выбор родительского элемента див
             parentDiv.classList.remove('changeOfStyle-fon');
+            currentMovie.watched = !currentMovie.watched; // Меняем состояние просмотра
+            saveMovieList();
         } else {
             btnClicked.classList.add('changeOfStyle-btn');      // Добавляем стили кнопке
             const titleEl = btnClicked.nextElementSibling;     // Следующий элемент (заголовок фильма)
             titleEl.classList.add('changeOfStyle-title');      // Добавляем стили заголовку
             const parentDiv = btnClicked.parentElement;       // Родительский div
             parentDiv.classList.add('changeOfStyle-fon');      // Добавляем стили фону родителя
+            currentMovie.watched = !currentMovie.watched; // Меняем состояние просмотра
+            saveMovieList();
         }}})
 
+// Загрузка стилей
+function loadStyle() {
+    movies.forEach((movie, index) => {
+        const itemLi = listofmoviesNode.children[index]; // Найти элемент фильма по его позиции
+        if (itemLi) {
+            const watchButton = itemLi.querySelector('.movie_title-btn-js');
+            const titleSpan = itemLi.querySelector('.movie_title');
+            const parentDiv = itemLi;
+            
+            if (movie.watched === true) {
+                watchButton.classList.add('changeOfStyle-btn');
+                titleSpan.classList.add('changeOfStyle-title');
+                parentDiv.classList.add('changeOfStyle-fon');
+            } else {
+                watchButton.classList.remove('changeOfStyle-btn');
+                titleSpan.classList.remove('changeOfStyle-title');
+                parentDiv.classList.remove('changeOfStyle-fon');
+            }
+        }
+    });
+}
 
 // Функция для удаления фильма
 function removeMovie(index) {
@@ -84,7 +138,7 @@ return movie;
 
 // Добавляем название фильма в массив
 function addMovie(movie) {
-    movies.push(movie);
+    movies.push({movie, watched: false});
 }
 
 
@@ -100,9 +154,15 @@ function renderMovie() {
         listMoviesHtml += `
             <div class="movie_title_list">
                 <button class = "movie_title-btn-js movie_title-btn" data-index="${index}"></button>
-                <p class="movie_title">${movie}</p>
+                <p class="movie_title">${movie.movie}</p>
                 <button class = "delete_movie-btn-js delete_movie-btn" data-index="${index}"></button>
             </div>
          ` });
          listofmoviesNode.innerHTML = listMoviesHtml;
+}
+
+// Сохраняем список в браузере
+function saveMovieList() {
+    const movieString = JSON.stringify(movies);
+    localStorage.setItem(MOVIE_STORAGE, movieString);
 }
